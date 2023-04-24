1f7059e1f92453dcbd223814fda08dd1c7234753
PlayVerv <PlayVerv@users.noreply.github.com>
2023-04-24 17:15:17 +0000

    deploy: a3e0573f343c42be499996cc05f362e66fee5ec3

--- index.search.js
+++ index.search.js
@@ -1,8 +1,8 @@
 var relearn_search_index=[{content:` 介面基本軟體運作畫面
-`,description:"基本介面配置說明",tags:null,title:"1. 基本介紹",uri:"/basic/index.html"},{content:` 軟體啟動後的畫面，此時尚未建立檔案，介面初始的狀態。
-`,description:"基本軟體運作畫面",tags:null,title:"介面",uri:"/basic/ui/index.html"},{content:` 歡迎使用 ANSI art creator ，以下簡稱AAC
+`,description:"基本介面配置說明",tags:null,title:"1. 基本介紹",uri:"/AAC.Manual/basic/index.html"},{content:` 軟體啟動後的畫面，此時尚未建立檔案，介面初始的狀態。
+`,description:"基本軟體運作畫面",tags:null,title:"介面",uri:"/AAC.Manual/basic/ui/index.html"},{content:` 歡迎使用 ANSI art creator ，以下簡稱AAC
 本軟體專為繪製ANSI art使用，主要目標bbs平台為批踢踢實業坊 (bbs)
 當前軟體提供的介面語系僅中文，儲存的.ans檔案也是以big5為編碼
 其餘語系及存檔格式尚待未來規劃製作。
 1. 基本介紹基本介面配置說明
-`,description:"",tags:null,title:"首頁",uri:"/index.html"},{content:"",description:"",tags:null,title:"Categories",uri:"/categories/index.html"},{content:" Youtube 繪製重播紀錄 ",description:"",tags:null,title:"Showcase",uri:"/more/showcase/index.html"},{content:"",description:"",tags:null,title:"Tags",uri:"/tags/index.html"}]
+`,description:"",tags:null,title:"首頁",uri:"/AAC.Manual/index.html"},{content:"",description:"",tags:null,title:"Categories",uri:"/AAC.Manual/categories/index.html"},{content:" Youtube 繪製重播紀錄 ",description:"",tags:null,title:"Showcase",uri:"/AAC.Manual/more/showcase/index.html"},{content:"",description:"",tags:null,title:"Tags",uri:"/AAC.Manual/tags/index.html"}]
