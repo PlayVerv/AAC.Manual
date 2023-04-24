bd6475155a05e803ab0cbca58c52257817b8181a
PlayVerv <PlayVerv@users.noreply.github.com>
2023-04-24 09:22:13 +0000

    deploy: c8cb05a0b9f90a8ccb3daafd5aeb3ff9de21f7e7

--- index.search.js
+++ index.search.js
@@ -1,1 +1,7 @@
-var relearn_search_index=[{content:"",description:"",tags:null,title:"AAC簡易說明文件",uri:"/AAC.Manual/index.html"},{content:"",description:"",tags:null,title:"Categories",uri:"/AAC.Manual/categories/index.html"},{content:"",description:"",tags:null,title:"Tags",uri:"/AAC.Manual/tags/index.html"}]
+var relearn_search_index=[{content:` 介面基本軟體運作畫面
+`,description:"",tags:null,title:"1. 基本介紹",uri:"/AAC.Manual/basic/index.html"},{content:" ",description:"基本軟體運作畫面",tags:null,title:"介面",uri:"/AAC.Manual/basic/ui/index.html"},{content:` 歡迎使用 ANSI art creator ，以下簡稱AAC
+本軟體專為繪製ANSI art使用，主要目標bbs平台為批踢踢實業坊 (bbs)
+當前軟體提供的介面語系僅中文，儲存的.ans檔案也是以big5為編碼
+其餘語系及存檔格式尚待未來規劃製作。
+1. 基本介紹介面基本軟體運作畫面
+`,description:"",tags:null,title:"首頁",uri:"/AAC.Manual/index.html"},{content:"",description:"",tags:null,title:"Categories",uri:"/AAC.Manual/categories/index.html"},{content:"",description:"",tags:null,title:"Tags",uri:"/AAC.Manual/tags/index.html"}]
