e0bc348ec0c09b4e179020ccc6ad141f8d3b651a
PlayVerv <PlayVerv@users.noreply.github.com>
2023-09-04 15:51:12 +0000

    deploy: bbad5b708e02e8f5570fc45a141df1766dce388d

--- index.search.js
+++ index.search.js
@@ -1,8 +1,29 @@
-var relearn_search_index=[{content:` 介面基本軟體運作畫面
-`,description:"基本介面配置說明",tags:null,title:"1. 基本介紹",uri:"/basic/index.html"},{content:` 軟體啟動後的畫面，此時尚未建立檔案，介面初始的狀態。
-`,description:"基本軟體運作畫面",tags:null,title:"介面",uri:"/basic/ui/index.html"},{content:` 歡迎使用 ANSI art creator ，以下簡稱AAC
+var relearn_search_index=[{content:"開啟PTT互動大圖拆圖工具 可以在上方Menu的工具，點選後選擇互動大圖分割 基本介面 操作 ",description:"拆圖工具，當圖片過大時，使用此工具把圖片拆成多個部分，並使用pmore互動指令來做為切換指令，適用平台為PTT。",tags:null,title:"PTT互動大圖拆圖",uri:"/tools/extra/pmoresplicescreen/index.html"},{content:` PTT互動大圖拆圖拆圖工具，當圖片過大時，使用此工具把圖片拆成多個部分，並使用pmore互動指令來做為切換指令，適用平台為PTT。
+符號編輯器編輯符號選擇器的工具，可自訂符號選擇器各Tab、群組、符號的配置及設定。
+`,description:"",tags:null,title:"特殊工具",uri:"/tools/extra/index.html"},{content:" 介面 繪製工具 選色器 符號選擇器 時間軸 歷史紀錄 圖層 屬性參數 ",description:"基本介面配置說明",tags:null,title:"1. 基本介紹",uri:"/basic/index.html"},{content:` 軟體啟動後的畫面，此時尚未建立檔案，介面初始的狀態。
+左方控制區塊 由上至下是
+繪製工具：筆刷、橡皮擦、框選等一般工具。 選色器：前景背景顏色設定，以及16/256/RGB顏色選擇。 符號選擇器：圖章工具塗刷時所使用的符號。 中間區塊 上方：主要繪製區域。 下方：影格控制。 右方控制區塊 由上至下是
+歷史紀錄：繪製動作紀錄的圖形化顯示。 圖層：基本繪製圖層的新增、移除、上下移動、顯示、鎖定等，以及固定圖層(參考圖、格線、背景色、洋蔥圖)的顯示。 參數設定：設定影格的參數或專案的參數。 `,description:"基本軟體運作畫面",tags:null,title:"介面",uri:"/basic/ui/index.html"},{content:`章節說明
+嘰哩呱啦
+`,description:"",tags:null,title:"Sub Content",uri:"/etc/second-content/index.html"},{content:` 特殊工具PTT互動大圖拆圖拆圖工具，當圖片過大時，使用此工具把圖片拆成多個部分，並使用pmore互動指令來做為切換指令，適用平台為PTT。 符號編輯器編輯符號選擇器的工具，可自訂符號選擇器各Tab、群組、符號的配置及設定。
+`,description:"繪製工具、編輯、輸出等工具的說明",tags:null,title:"2. 工具介紹",uri:"/tools/index.html"},{content:"開啟符號編輯器 可以在上方Menu的工具，點選後選擇編輯符號列表 或是在符號選擇的左上方，有一個編輯按鈕，點選後開啟 基本介面 操作 ",description:"編輯符號選擇器的工具，可自訂符號選擇器各Tab、群組、符號的配置及設定。",tags:null,title:"符號編輯器",uri:"/tools/extra/glypheditor/index.html"},{content:` 當前一般圖層所提供的繪製工具。 由左至右分別為
+上色筆刷 (塗刷前景色或背景色) 顏料桶 (大範圍變更前景或背景色) 符號圖章 (使用符號選擇器所選的符號來繪製) 橡皮擦 (清除前景符號或背景顏色) 矩形選擇 (選擇框，選擇後可範圍改變前景背景色、清除，或複製ANSI資料) 段落型選擇 (選擇框，選擇後可範圍改變前景背景色、清除，或複製ANSI資料) 文字輸入 (文字輸入工具) `,description:"選擇繪製工具介面說明",tags:null,title:"繪製工具",uri:"/basic/toolpicker/index.html"},{content:` 前景、背景顏色設定。 上方前景跟背景按鈕為當前設定的前景跟背景色，紅色框為當前選擇目標，變更顏色會變更此。
+顏色選擇。 當前Tab提供16色、256色、RGB的選項，點選顏色後，上方紅色選擇框的目標即會設定為此顏色。
+而16色選擇較為特殊，有特別分前景跟背景色，不受上方紅色選擇框影響，點前景色圓球即會改變上方前景色方塊，點選背景色圓球即會改變上方背景色方塊，不論上方紅色選擇框線當前選擇哪個。
+`,description:"選色器介面說明",tags:null,title:"選色器",uri:"/basic/colorpicker/index.html"},{content:" ",description:"偏好設定功能及說明",tags:null,title:"3. 偏好設定",uri:"/preference/index.html"},{content:" ",description:"符號選擇器介面說明",tags:null,title:"符號選擇器",uri:"/basic/glyphpicker/index.html"},{content:" ",description:"動手開始繪製一張ANSI圖",tags:null,title:"4. 開始繪製",uri:"/draw/index.html"},{content:"",description:"時間軸說明",tags:null,title:"時間軸",uri:"/basic/timeline/index.html"},{content:" ",description:"繪製完成後輸出.ans存檔，或是輸出圖片、影片等說明",tags:null,title:"5. 輸出",uri:"/export/index.html"},{content:"",description:"歷史紀錄說明",tags:null,title:"歷史紀錄",uri:"/basic/history/index.html"},{content:` Sub Content章節說明 嘰哩呱啦
+`,description:"一些小工具或規劃等等",tags:null,title:"6. 其他",uri:"/etc/index.html"},{content:"",description:"圖層說明",tags:null,title:"圖層",uri:"/basic/layer/index.html"},{content:"",description:"屬性參數說明",tags:null,title:"屬性參數",uri:"/basic/property/index.html"},{content:` 歡迎使用 ANSI art creator ，以下簡稱AAC
 本軟體專為繪製ANSI art使用，主要目標bbs平台為批踢踢實業坊 (bbs)
 當前軟體提供的介面語系僅中文，儲存的.ans檔案也是以big5為編碼
 其餘語系及存檔格式尚待未來規劃製作。
 1. 基本介紹基本介面配置說明
-`,description:"",tags:null,title:"首頁",uri:"/index.html"},{content:"",description:"",tags:null,title:"Categories",uri:"/categories/index.html"},{content:" Youtube 繪製重播紀錄 ",description:"",tags:null,title:"Showcase",uri:"/more/showcase/index.html"},{content:"",description:"",tags:null,title:"Tags",uri:"/tags/index.html"}]
+2. 工具介紹繪製工具、編輯、輸出等工具的說明
+3. 偏好設定偏好設定功能及說明
+4. 開始繪製動手開始繪製一張ANSI圖
+5. 輸出繪製完成後輸出.ans存檔，或是輸出圖片、影片等說明
+6. 其他一些小工具或規劃等等
+`,description:"",tags:null,title:"首頁",uri:"/index.html"},{content:` 1234
+Hello World
+Lilicoco
+註釋 A notice disclaimer
+提示 A tip disclaimer
+`,description:"",tags:null,title:"QQ",uri:"/etc/second-content/qq/index.html"},{content:"",description:"",tags:null,title:"類別",uri:"/categories/index.html"},{content:" Youtube 筆刷重播紀錄影片，繪製過程使用筆刷記錄功能，完整繪製後重播記錄檔。 ",description:"",tags:null,title:"Showcase",uri:"/more/showcase/index.html"},{content:"",description:"",tags:null,title:"標籤",uri:"/tags/index.html"}]
